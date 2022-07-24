ed0694151c2c5fe4ae4fb42af26dddcce46b731a
Dhanush <90715682+ydhanush8@users.noreply.github.com>
2022-07-24 15:23:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// For first dice
+
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+
+var randomDiceImage = "dice"+randomNumber1+".png";
+
+var randomDiceSource = "images/"+randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomDiceSource);
+
+// For second dice
+
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+
+var randomDiceImage2 = "dice"+randomNumber2+".png";
+
+var randumDiceSource2 = "images/"+randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randumDiceSource2);
+
+// Comparing dice numbers
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "First Player Wins";
+}
+else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML ="Second Player Wins";
+}
+else{
+  document.querySelector("h1").innerHTML ="Draw";
+}
